7e1ee525d8bcc87a03da0464c56b0443191d55f5
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2023-08-23 16:20:31 -0300

    1

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(responseTime())
 
 app.get('/', async (_req, res) => {
 
-  // const cachedValue = await redis.get('users')
-  // if(cachedValue) {
-  //   console.log('Fetching from cache')
-  //   return res.json(JSON.parse(cachedValue))
-  // }
+  const cachedValue = await redis.get('users')
+  if(cachedValue) {
+    console.log('Fetching from cache')
+    return res.json(JSON.parse(cachedValue))
+  }
 
   const users = await prisma.users.findMany({
     include: {
@@ -26,6 +26,20 @@ app.get('/', async (_req, res) => {
 
   console.log('Fetching from DB')
   await redis.set('users', JSON.stringify(users))
+  // await redis.rpush('comments', JSON.stringify(users))
+  res.json(users)
+})
+
+app.get('/sinredis', async (_req, res) => {
+
+  const users = await prisma.users.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+
+  console.log('Fetching from DB')
   // await redis.rpush('comments', JSON.stringify(users))
   res.json(users)
 })
